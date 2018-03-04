2679bfb89b076f7bfe65ec83c97b138532fe354a
John Liu <johnliu55tw@gmail.com>
2018-03-04 16:31:06 +0800

    Added actual weather response.

--- app.py
+++ app.py
@@ -12,12 +12,20 @@ WEATHER_API_KEY = '337b098c340f425985871125180403'
 
 @app.route('/', methods=['POST'])
 def index():
-    logging.info('Query parameters: {}'.format(request.args))
-    logging.info('Query JSON body: {}'.format(request.get_json()))
-
-    q_response = 'This is a sample response from your webhook!'
-    resp = {'speech': q_response, 'displayText': q_response}
-    return jsonify(resp)
+    try:
+        json_body = request.get_json()
+        geo_city = json_body['result']['parameters']['geo-city']
+        if not geo_city:
+            raise ValueError('You must provide location!')
+        date = json_body['result']['parameters'].get('date', '')
+        logging.info("geo-city: '{}', date: {}.".format(
+            geo_city, date))
+        weather_expr = utils.get_weather_expression(WEATHER_API_KEY, geo_city, date)
+        return jsonify({'speech': weather_expr, 'displayText': weather_expr})
+    except Exception as e:
+        logging.exception('Error occurred in index.')
+        return jsonify({'speech': 'Error occurred: {}'.format(e),
+                        'displayText': 'Error occurred: {}'.format(e)})
 
 
 if __name__ == '__main__':
